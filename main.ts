4401b30f1b9c37904e345d1102505659e6cff587
Elea2008 <103181955+Elea2008@users.noreply.github.com>
2022-05-19 10:57:22 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,69 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) < -100) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.Y) > 100) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) < -100) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) > 100) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.Y) < -1023) {
+        basic.showLeds(`
+            . # # # #
+            . . . # #
+            . . # . #
+            . # . . #
+            # . . . .
+            `)
+    } else if (input.acceleration(Dimension.X) == -1023) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+    } else if (input.acceleration(Dimension.Y) == 1023) {
+        basic.showLeds(`
+            # . . . .
+            . # . . #
+            . . # . #
+            . . . # #
+            . # # # #
+            `)
+    } else if (input.acceleration(Dimension.X) == -1023) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+})
